0581b1345de1823faf2ef03f212974c463143919
Maciej Witkowski <maciejwitkowski91@gmail.com>
2024-03-24 21:37:26 +0100

    Add MainModuleProvider content

--- app/src/main/java/maciej/witkowski/koleorecruitmenttask/MainModuleProvider.kt
+++ app/src/main/java/maciej/witkowski/koleorecruitmenttask/MainModuleProvider.kt
@@ -1,10 +1,27 @@
 package maciej.witkowski.koleorecruitmenttask
 
+import android.system.Os.bind
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.util.concurrent.TimeUnit
+import maciej.witkowski.koleorecruitmenttask.common.DispatcherProvider
+import maciej.witkowski.koleorecruitmenttask.common.DispatcherProviderImpl
+import maciej.witkowski.koleorecruitmenttask.data.StationsApiService
+import maciej.witkowski.koleorecruitmenttask.data.StationsRepositoryImpl
+import maciej.witkowski.koleorecruitmenttask.domain.CombineStationsUseCase
+import maciej.witkowski.koleorecruitmenttask.domain.CombineStationsUseCaseImpl
+import maciej.witkowski.koleorecruitmenttask.domain.GetKeywordsUseCase
+import maciej.witkowski.koleorecruitmenttask.domain.GetKeywordsUseCaseImpl
+import maciej.witkowski.koleorecruitmenttask.domain.GetStationsUseCase
+import maciej.witkowski.koleorecruitmenttask.domain.GetStationsUseCaseImpl
+import maciej.witkowski.koleorecruitmenttask.domain.StationsRepository
+import maciej.witkowski.koleorecruitmenttask.ui.MainViewModel
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -15,6 +32,14 @@ internal val BASE_URL_QUALIFIER = named("BASE_URL")
 object MainModuleProvider : ModuleProvider {
 
     override fun get() = module {
+
+        singleOf(StationsApiService::invoke)
+        factoryOf(::StationsRepositoryImpl) { bind<StationsRepository>() }
+        factoryOf(::CombineStationsUseCaseImpl) { bind<CombineStationsUseCase>() }
+        factoryOf(::GetStationsUseCaseImpl) { bind<GetStationsUseCase>() }
+        factoryOf(::GetKeywordsUseCaseImpl) { bind<GetKeywordsUseCase>() }
+        viewModelOf(::MainViewModel)
+        single<DispatcherProvider> { DispatcherProviderImpl() }
 
         single {
             provideRetrofit(
