007c0d1392ff2e869df521cd3bc306842f3d4254
Maciej Witkowski <maciejwitkowski91@gmail.com>
2024-03-24 15:07:14 +0100

    Add GetStationsUseCase

---
+++ app/src/main/java/maciej/witkowski/koleorecruitmenttask/domain/GetStationsUseCaseImpl.kt
@@ -0,0 +1,16 @@
+package maciej.witkowski.koleorecruitmenttask.domain
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import maciej.witkowski.koleorecruitmenttask.data.StationsItem
+import maciej.witkowski.koleorecruitmenttask.domain.base.BaseObservableUseCase
+
+class GetStationsUseCaseImpl(
+    private val stationsRepository: StationsRepository,
+) : GetStationsUseCase, BaseObservableUseCase<Unit, List<StationsItem>>() {
+
+    override fun createFlow(request: Unit): Flow<List<StationsItem>> = flow {
+        emitAll(stationsRepository.getStations())
+    }
+}
